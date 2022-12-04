66aa1ae4d8113ad0715981ea7a91f8f927fb5dcb
swidodo <smtwidodo04@gmail.com>
2022-12-04 23:29:55 +0700

    update eldiv

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,25 @@
+  <!-- ======= Header ======= -->
+ <!-- End Top Bar -->
+
+ <header id="header" class="header d-flex align-items-center">
+
+<div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+  <a href="index.html" class="logo d-flex align-items-center">
+    <!-- Uncomment the line below if you also wish to use an image logo -->
+    <!-- <img src="assets/img/logo.png" alt=""> -->
+    <img src="assets/img/eldiv.png" alt="eldiv" class="">
+  </a>
+  <nav id="navbar" class="navbar">
+    <ul>
+      <li><a href="projects.html">Projects</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav><!-- .navbar -->
+
+  <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+  <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+</div>
+</header><!-- End Header -->
+<!-- End Header -->
